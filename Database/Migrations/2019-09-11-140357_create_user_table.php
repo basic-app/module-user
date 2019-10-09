5777873a3106c2b361a8beb955db4080b86f415e
basic-app <dev@basic-app.com>
2019-10-09 10:35:25 +0300

    n/a

--- Database/Migrations/2019-09-11-140357_create_user_table.php
+++ Database/Migrations/2019-09-11-140357_create_user_table.php
@@ -7,4 +7,19 @@ namespace BasicApp\User\Database\Migrations;
 
 class CreateUserTable extends \denis303\user\CreateUserTableMigration
 {
+
+    public function getFields()
+    {
+        $return = parent::getFields();
+
+        $return[static::FIELD_PREFIX . 'enabled'] = [
+            'type' => 'TINYINT',
+            'constraint' => 1,
+            'null' => false,
+            'default' => 1
+        ];
+
+        return $return;
+    }
+
 }
