0535146cb16970a9e0718e2f0a6e7b6869df92e5
basic-app <dev@basic-app.com>
2020-02-18 21:40:36 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -4,11 +4,11 @@
  * @license MIT
  * @link http://basic-app.com
  */
-$routes->add('admin/user', 'BasicApp\User\Controllers\Admin\User::index');
-$routes->add('admin/user/(:segment)', 'BasicApp\User\Controllers\Admin\User::$1');
+$routes->add('admin/user', '\BasicApp\User\Controllers\Admin\User::index');
+$routes->add('admin/user/(:segment)', '\BasicApp\User\Controllers\Admin\User::$1');
 
-$routes->add('member', 'BasicApp\User\Controllers\Member\User::index');
-$routes->add('member/(:segment)', 'BasicApp\User\Controllers\Member\User::$1');
+$routes->add('member', '\BasicApp\User\Controllers\Member\User::index');
+$routes->add('member/(:segment)', '\BasicApp\User\Controllers\Member\User::$1');
 
-$routes->add('user/(:segment)', 'BasicApp\User\Controllers\User::$1');
-$routes->add('user/(:segment)/(:any)', 'BasicApp\User\Controllers\User::$1/$2');
+$routes->add('user/(:segment)', '\BasicApp\User\Controllers\User::$1');
+$routes->add('user/(:segment)/(:any)', '\BasicApp\User\Controllers\User::$1/$2');
